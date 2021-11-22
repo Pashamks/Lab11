84e2a25015140279eb41cc90b6d30f38a4f07792
pavlo <pavlo@DESKTOP-SM098C1>
2021-11-22 15:39:54 +0200

    version without stocke price

--- Program.cs
+++ Program.cs
@@ -6,23 +6,31 @@ namespace Server
 {
     class Program
     {
+        static DateTime timeWeather = DateTime.Now, timeRates = DateTime.Now, timeStockePrice = DateTime.Now;
         static void Main(string[] args)
         {
-            DateTime timeWeather = DateTime.Now, timeRates = DateTime.Now;
-           
             while (true)
             {
                 if(Math.Abs(timeRates.Day - DateTime.Now.Day) == 0)
                 {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine($"{DateTime.Now} Rate checker is working...");
                     timeRates = timeRates.AddDays(1);
                     RatesManager.CheckRates();
                 }
-                if (Math.Abs(timeWeather.Minute - DateTime.Now.Minute) == 1)
+                if (Math.Abs(timeWeather.Hour - DateTime.Now.Hour) == 0)
                 {
+                    Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine($"{DateTime.Now} Weather checker is working...");
-                    timeWeather = DateTime.Now;
+                    timeWeather = timeWeather.AddHours(1);
                     WeatherManager.CheckWeather();
+                }
+                if (Math.Abs(timeStockePrice.Minute - DateTime.Now.Minute) == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"{DateTime.Now} Stocke price checker is working...");
+                    timeStockePrice = timeStockePrice.AddMinutes(1);
+
                 }
             }
         }
